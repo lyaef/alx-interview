d0b0f881fea20211fdca338ebee305159be94642
lyaef <lyaef58@gmail.com>
2023-02-13 00:07:08 +0300

    Done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,27 @@
 #!/usr/bin/node
-
 const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const FILMID = process.argv[2];
 
-const id = process.argv[2] + '/';
-const url = 'https://swapi-api.hbtn.io/api/films/';
-
-request(url + id, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  const listUrl = JSON.parse(body).characters;
-
-  for (const li of listUrl) {
-    await new Promise(function (resolve, reject) {
-      request(li, function (err, res, body) {
-        if (err) return console.error(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
+function doRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
     });
+  });
+}
+
+async function main (filmId) {
+  const res = await doRequest(`${url}/${filmId}`);
+  for (const character of res.characters) {
+    const resCharcter = await doRequest(character);
+    console.log(resCharcter.name);
   }
-});
+}
+
+main(FILMID);
+Footer
